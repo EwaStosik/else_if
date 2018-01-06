9c94c91156eaec947cb2321c61454d1357bc8db0
Ewa Stosik <e.stosik@outlook.com>
2018-01-06 16:27:11 +0100

    first correction

--- else_if.js
+++ else_if.js
@@ -1,12 +1,12 @@
 var a = 7;
 var b = 4;
 var equation = (a * a) - (2 * a * b) + (b * b);
-if (equation >= 0) {
+if (equation > 0) {
     console.log('Wynik dodatni');
 } 
 else if (equation == 0) {
     console.log('Wynik to 0');
 }
-else if (equation <= 0) {
+else if (equation < 0) {
     console.log('Wynik ujemny');
 } 
